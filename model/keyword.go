35d807fa5326849fd83710cfec7db761c153428a
Sam Helman <sam.helman@10gen.com>
2014-04-10 16:29:03 -0400

    Random keywords as landing works

--- model/keyword.go
+++ model/keyword.go
@@ -3,6 +3,7 @@ package model
 import (
 	"fmt"
 	"labix.org/v2/mgo/bson"
+	"math/rand"
 	"shelman/sourgrapes/db"
 )
 
@@ -14,6 +15,45 @@ type Keyword struct {
 	Id     bson.ObjectId `bson:"_id,omitempty"`
 	Word   string        `bson:"kw"`
 	Movies []string      `bson:"mv"`
+}
+
+func GetRandomKeywords(howMany int) ([]Keyword, error) {
+	sess, d, err := db.GetFactory().GetSession()
+	if err != nil {
+		return nil, fmt.Errorf("couldn't get session: %v", err)
+	}
+	defer sess.Close()
+
+	total, err := d.C(KEYWORD_COLLECTION).Count()
+	if err != nil {
+		return nil, fmt.Errorf("error counting keywords: %v", err)
+	}
+
+	toRet := []Keyword{}
+	for i := 0; i < howMany; i++ {
+		toSkip := rand.Int() % total
+		out := &Keyword{}
+		err = d.C(KEYWORD_COLLECTION).Find(bson.M{}).Select(bson.M{"kw": 1}).
+			Skip(toSkip).One(out)
+		if err != nil {
+			return nil, fmt.Errorf("error finding keyword: %v", err)
+		}
+		toRet = append(toRet, *out)
+	}
+
+	return toRet, nil
+}
+
+func FindKeyword(word string) (*Keyword, error) {
+	sess, d, err := db.GetFactory().GetSession()
+	if err != nil {
+		return nil, fmt.Errorf("couldn't get session: %v", err)
+	}
+	defer sess.Close()
+
+	keyword := &Keyword{}
+	err = d.C(KEYWORD_COLLECTION).Find(bson.M{"kw": word}).One(keyword)
+	return keyword, err
 }
 
 func FindKeywords(words []string) ([]Keyword, error) {
--- server/server.go
+++ server/server.go
@@ -18,18 +18,41 @@ func Start() {
 	m := martini.Classic()
 
 	m.Get("/", indexHandler)
+	m.Get("/keyword/:word", keywordHandler)
 	m.Get("/movie/:title", movieHandler)
 
 	m.Run()
 }
 
 func indexHandler(res http.ResponseWriter, req *http.Request) {
+
+	randomKeywords, err := model.GetRandomKeywords(15)
+	if err != nil {
+		res.Write([]byte(fmt.Sprintf("error: %v", err)))
+	}
+
 	tmpl, err := template.ParseFiles(filepath.Join(tmplRoot, "main.html"))
 	if err != nil {
 		res.Write([]byte(fmt.Sprintf("error: %v", err)))
 		return
 	}
-	tmpl.Execute(res, nil)
+	tmpl.Execute(res, randomKeywords)
+}
+
+func keywordHandler(params martini.Params, res http.ResponseWriter, req *http.Request) {
+	word := params["word"]
+	keyword, err := model.FindKeyword(word)
+	if err != nil {
+		res.Write([]byte(fmt.Sprintf("error: %v", err)))
+		return
+	}
+
+	tmpl, err := template.ParseFiles(filepath.Join(tmplRoot, "keyword.html"))
+	if err != nil {
+		res.Write([]byte(fmt.Sprintf("error: %v", err)))
+		return
+	}
+	tmpl.Execute(res, keyword)
 }
 
 func movieHandler(params martini.Params) string {
